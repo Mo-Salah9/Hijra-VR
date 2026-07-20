c7663efd729022a641e5efc6dfab2442361a6043
mohammad salah <msalah1999@gmail.com>
2026-07-21 01:01:34 +0300

    hh

--- Assets/Scripts/secene3manager.cs
+++ Assets/Scripts/secene3manager.cs
@@ -10,6 +10,9 @@ using UnityEngine.Video;
 
 public class secene3manager : MonoBehaviour
 {
+    public Animator animator1;
+    public  Animator animator2;
+    public GameObject horse;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,6 +32,13 @@ public class secene3manager : MonoBehaviour
         AudioManager.Instance.PlayAlone("0");
         yield return new WaitForSeconds(14);
         AudioManager.Instance.PlayAlone("1");
+        yield return new WaitForSeconds(24);
+        AudioManager.Instance.PlayAlone("2");
+        yield return new WaitForSeconds(7);
+        animator1.SetBool("go", true);
+        animator2.SetBool("go", true);
+        yield return new WaitForSeconds(7);
+        horse.SetActive(false);
         yield return null;
     }
 }
